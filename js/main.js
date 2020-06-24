d920721bcf68e54a4b2bf79c26f67ae40a1add21
ishitb <ib370@snu.edu.in>
2020-06-25 01:48:11 +0530

    expressions can now be detected

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
-const video = document.querySelector("#video");
+const video = document.getElementById("video");
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("js/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("js/models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("js/models"),
+]).then(getVideo);
 
 function getVideo() {
     navigator.getUserMedia(
         { video: {} },
-        (stream) => video.srcObject = stream,
-        (err) => console.error(err)
+        (stream) => (video.srcObject = stream),
+        (e) => console.error(e)
     );
-    console.log(video, "hello");
 }
 
-getVideo();
+video.addEventListener("play", () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(async () => {
+        const detections = await faceapi
+            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceExpressions();
+        // console.log(detections)
+        const expressions = detections.pop()['expressions']
+        console.log(expressions)
+    }, 100);
+});
