dc75851dcccb0887a63d8d796bad15aaf3de06d8
ishitb <ib370@snu.edu.in>
2020-06-30 00:51:57 +0530

    added expression tracker and emoji changing. only sounds left

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 const video = document.getElementById("video");
+const happyDiv = document.querySelector(".happy");
+const angryDiv = document.querySelector(".angry");
+const disgustedDiv = document.querySelector(".disgusted");
+const feardulDiv = document.querySelector(".feardul");
+const neutralDiv = document.querySelector(".neutral");
+const sadDiv = document.querySelector(".sad");
+const surprisedDiv = document.querySelector(".surprised");
+const notVisibleDiv = document.querySelector(".notVisible")
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri("js/models"),
@@ -14,17 +22,46 @@ function getVideo() {
     );
 }
 
+getExpression = (expressions) => {
+    let face = "neutral";
+    for (const expression in expressions) {
+        if (expressions[face] < expressions[expression])
+            face = expression;
+    }
+    return face;
+};
+
+showExpression = (divs, expression) => {
+    for (const div in divs) {
+        divs[div].style.display = "none"
+    }
+    divs[expression].style.display = "flex"
+}
+
 video.addEventListener("play", () => {
     const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
     const displaySize = { width: video.width, height: video.height };
     faceapi.matchDimensions(canvas, displaySize);
+    let expressions;
+    let divs = {
+        'happy': happyDiv,
+        'neutral': neutralDiv,
+        'sad': sadDiv,
+        'disgusted': disgustedDiv,
+        'angry': angryDiv,
+        'surprised': surprisedDiv,
+        'notVisible': notVisibleDiv
+    };
     setInterval(async () => {
         const detections = await faceapi
             .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
             .withFaceExpressions();
-        // console.log(detections)
-        const {angry, digusted, fearful, happy, neutral, sad, surprised} = detections.pop()['expressions']
-        console.log(happy)
+
+        if (detections.length != 0) {
+            const {happy, neutral, disgusted, sad, fearful, surprised, angry} = detections.pop()["expressions"];
+            expressions = {happy, neutral, disgusted, sad, fearful, surprised, angry};
+            const faceExpression = getExpression(expressions);
+            showExpression(divs, faceExpression)
+        } else showExpression(divs, 'notVisible');
     }, 100);
 });
